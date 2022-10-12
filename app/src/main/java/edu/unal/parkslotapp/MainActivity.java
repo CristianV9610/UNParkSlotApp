39cb8870a1b7fe85746080b0380874023fced8ab
andgonzalezmar <37084527+andgonzalezmar@users.noreply.github.com>
2022-10-11 22:44:17 -0500

    Se agrega el Binding

--- app/src/main/java/edu/unal/parkslotapp/MainActivity.java
+++ app/src/main/java/edu/unal/parkslotapp/MainActivity.java
@@ -3,12 +3,19 @@ package edu.unal.parkslotapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import edu.unal.parkslotapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
