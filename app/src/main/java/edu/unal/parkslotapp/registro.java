73290daf109ee596bcc1768f27f9d1aa1197adad
emmendezp <emmendezp@unal.edu.co>
2022-10-19 22:54:12 -0500

    cambios base

--- app/src/main/java/edu/unal/parkslotapp/registro.java
+++ app/src/main/java/edu/unal/parkslotapp/registro.java
@@ -1,12 +1,18 @@
 package edu.unal.parkslotapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,8 +21,9 @@ import java.security.PublicKey;
 public class registro extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText signinEmail;
-    private EditText signinUser;
+    //private EditText signinUser;
     private EditText signinPassword;
+    private EditText signinPassword1;
 
 
     @Override
@@ -25,8 +32,8 @@ public class registro extends AppCompatActivity {
         setContentView(R.layout.win_dialog_siginin);
         mAuth = FirebaseAuth.getInstance();
         signinEmail = findViewById(R.id.signinEmail);
-        signinUser = findViewById(R.id.signinUser);
         signinPassword = findViewById(R.id.signinPassword);
+        signinPassword1 = findViewById(R.id.signinPassword1);
 
     }
     @Override
@@ -37,30 +44,34 @@ public class registro extends AppCompatActivity {
         //updateUI(currentUser);
     }
     public void onloggClick(View view) {
-        Intent intent = new Intent(this, Login.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
     }
     public void registroUsuario(View view){
-        /*
-        mAuth.createUserWithEmailAndPassword()
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCustomToken:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
-                            Toast.makeText(CustomAuthActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+        if (signinPassword.getText().toString().equals(signinPassword1.getText().toString())){
+            mAuth.createUserWithEmailAndPassword(signinEmail.getText().toString(), signinPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(getApplicationContext(), "Usuario creado con exito .", Toast.LENGTH_SHORT).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                              //  updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(getApplicationContext(), "Registro fallido !!.", Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
                         }
-                    }
-                });*/
+                    });
+        }else{
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
+
 
     }
 
